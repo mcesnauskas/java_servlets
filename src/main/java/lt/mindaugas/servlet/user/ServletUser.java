1aac8054358b601b1e2a3320e933079611032ce2
Mindaugas <mcesnauskas@gmail.com>
2024-04-02 09:09:40 +0300

    Add example POST User details with form

--- src/main/java/lt/mindaugas/servlet/user/ServletUser.java
+++ src/main/java/lt/mindaugas/servlet/user/ServletUser.java
@@ -46,5 +46,27 @@ public class ServletUser extends HttpServlet {
         writer.write(HtmlTemplate.htmlEnd);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
 
+        User user = new User(
+                Math.incrementExact(Integer.parseInt(req.getParameter("userId"))),
+                req.getParameter("firstName"),
+                req.getParameter("lastName"),
+                req.getParameter("email"),
+                req.getParameter("phone")
+        );
+
+        writer.write(HtmlTemplate.htmlStart.formatted("User Details"));
+        writer.write("<h>NEW USER WAS CREATED</h1>");
+        writer.write("<p>Id: %s</p>".formatted(user.id()));
+        writer.write("<p>First name: %s</p>".formatted(user.firstName()));
+        writer.write("<p>Last name: %s</p>".formatted(user.lastName()));
+        writer.write("<p>Email: %s</p>".formatted(user.email()));
+        writer.write("<p>Phone: %s</p>".formatted(user.phone()));
+
+        writer.write(HtmlTemplate.htmlEnd);
+    }
 }
